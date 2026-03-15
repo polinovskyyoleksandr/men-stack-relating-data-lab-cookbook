2b4681c6096bf102e04047ab9fc913b189317f65
polino <polinovskyoleksandr1303@gmail.com>
2026-03-15 13:09:40 +0100

    another try

--- controllers/foods.js
+++ controllers/foods.js
@@ -4,7 +4,7 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId);
+    const user = await User.findById(req.session.user._id);
     res.render('foods/index.ejs', {
       pantry: user.pantry,
       owner: user,
@@ -17,22 +17,25 @@ router.get('/', async (req, res) => {
 
 
 router.get('/new', (req, res) => {
-  if (req.params.userId !== req.session.user._id) {
-    return res.redirect(`/users/${req.params.userId}/foods`);
-  }
   res.render('foods/new.ejs');
 });
 
 
 router.post('/', async (req, res) => {
   try {
-    if (req.params.userId !== req.session.user._id) {
-      return res.redirect('/');
-    }
-    const user = await User.findById(req.params.userId);
+
+    //console.log("SESSION USER:", req.session.user)
+
+    const user = await User.findById(req.session.user._id);
+
+    //console.log("DB USER:", user)
+
     user.pantry.push(req.body);
+
     await user.save();
+
     res.redirect(`/users/${user._id}/foods`);
+
   } catch (error) {
     console.log(error);
     res.redirect('/');
@@ -42,10 +45,7 @@ router.post('/', async (req, res) => {
 
 router.get('/:itemId/edit', async (req, res) => {
   try {
-    if (req.params.userId !== req.session.user._id) {
-      return res.redirect('/');
-    }
-    const user = await User.findById(req.params.userId);
+    const user = await User.findById(req.session.user._id);
     const food = user.pantry.id(req.params.itemId);
     res.render('foods/edit.ejs', { food });
   } catch (error) {
@@ -56,13 +56,10 @@ router.get('/:itemId/edit', async (req, res) => {
 
 router.put('/:itemId', async (req, res) => {
   try {
-    if (req.params.userId !== req.session.user._id) {
-      return res.redirect('/');
-    }
-    const user = await User.findById(req.params.userId);
+    const user = await User.findById(req.session.user._id);
     const food = user.pantry.id(req.params.itemId);
-    food.set(req.body)
-    await user.save()
+    food.set(req.body);
+    await user.save();
     res.redirect(`/users/${user._id}/foods`);
   } catch (error) {
     console.log(error);
@@ -72,16 +69,13 @@ router.put('/:itemId', async (req, res) => {
 
 router.delete('/:itemId', async (req, res) => {
   try {
-    if (req.params.userId !== req.session.user._id) {
-      return res.redirect('/');
-    }
-    const user = await User.findById(req.params.userId);
+    const user = await User.findById(req.session.user._id);
     user.pantry.id(req.params.itemId).deleteOne();
     await user.save();
     res.redirect(`/users/${user._id}/foods`);
   } catch (error) {
-    console.log(error)
-    res.redirect('/')
+    console.log(error);
+    res.redirect('/');
   }
 });
 
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const foodsController = require('./controllers/foods.js');
+const usersController = require('./controllers/users.js');
 
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -54,6 +55,7 @@ app.get('/vip-lounge', (req, res) => {
 app.use(passUserToView);
 app.use('/auth', authController);
 app.use(isSignedIn);
+app.use('/users', usersController);
 app.use('/users/:userId/foods', foodsController);
 
 app.listen(port, () => {
